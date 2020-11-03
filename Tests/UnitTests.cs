a23ed8a3ef5b190d0c9caad72dc6dae5506bd9af
Tomo Bessho <tbessho2@unl.edu>
2020-11-03 08:57:20 -0600

    failed test

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
+using System;
 
 namespace TravisCILab
 {
@@ -31,5 +31,28 @@ namespace TravisCILab
         }
 
         // Implement 3 tests per operation, following a similar pattern as above
+        [Test]
+        public void Pow_Valid()
+        {
+            Assert.AreEqual(3, Program.Power("1", "2"));
+            Assert.AreEqual(5, Program.Power("3", "2"));
+            Assert.AreEqual(12, Program.Power("5", "7"));
+        }
+
+        [Test]
+        public void Pow_Invalid()
+        {
+            Assert.Throws<FormatException>(() => Program.Power("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "a"));
+        }
+
+        [Test]
+        public void Pow_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Power("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, null));
+        }
     }
 }
